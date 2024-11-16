57bb7a34063600bea6655a861d2fbbe9fad744c6
Moutain <erbynn1234@gmail.com>
2024-11-16 14:30:18 +0000

    hmm

--- src/routes/qrcode.js
+++ src/routes/qrcode.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/generate-qrcode', async (req, res) => {
     try {
-        const serverUrl = `https://z7tl0djt-3000.euw.devtunnels.ms`
+        const serverUrl = `${req.protocol}://${req.headers.host}`;
         const qrCode = await qrcode.toDataURL(serverUrl)
         res.send(`<img src="${qrCode}" width=500 alt="QR Code"/>`)
 
